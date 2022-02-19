448a19267e4db0131bc855a3de47fe1d9bf91338
laszlo-ratesic <keenanrc@hotmail.com>
2022-02-19 11:36:28 -0600

    add security features

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,13 +5,20 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-import { StringUtils } from "./libraries/StringUtils.sol";
-import { Base64 } from "./libraries/Base64.sol";
+import {StringUtils} from "./libraries/StringUtils.sol";
+import {Base64} from "./libraries/Base64.sol";
 
 import "hardhat/console.sol";
 
+error Unauthorized();
+error AlreadyRegistered();
+error InvalidName(string name);
+
 // Inheriting imported contract gives access to inherited methods
-contract Domains is ERC721URIStorage{
+contract Domains is ERC721URIStorage {
+    // global owner variable
+    address payable public owner;
+
     // Keeps track of token IDs
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -20,25 +27,40 @@ contract Domains is ERC721URIStorage{
     string public tld;
 
     // NFT images will be stored as on-chain SVGs
-    string svgPartOne = '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#a)" d="M0 0h270v270H0z"/><defs><filter id="b" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949a4.382 4.382 0 0 0-4.394 0l-10.081 6.032-6.85 3.934-10.081 6.032a4.382 4.382 0 0 1-4.394 0l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616 4.54 4.54 0 0 1-.608-2.187v-9.31a4.27 4.27 0 0 1 .572-2.208 4.25 4.25 0 0 1 1.625-1.595l7.884-4.59a4.382 4.382 0 0 1 4.394 0l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616 4.54 4.54 0 0 1 .608 2.187v6.032l6.85-4.065v-6.032a4.27 4.27 0 0 0-.572-2.208 4.25 4.25 0 0 0-1.625-1.595L41.456 24.59a4.382 4.382 0 0 0-4.394 0l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595 4.273 4.273 0 0 0-.572 2.208v17.441a4.27 4.27 0 0 0 .572 2.208 4.25 4.25 0 0 0 1.625 1.595l14.864 8.655a4.382 4.382 0 0 0 4.394 0l10.081-5.901 6.85-4.065 10.081-5.901a4.382 4.382 0 0 1 4.394 0l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616 4.54 4.54 0 0 1 .608 2.187v9.311a4.27 4.27 0 0 1-.572 2.208 4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721a4.382 4.382 0 0 1-4.394 0l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616 4.53 4.53 0 0 1-.608-2.187v-6.032l-6.85 4.065v6.032a4.27 4.27 0 0 0 .572 2.208 4.25 4.25 0 0 0 1.625 1.595l14.864 8.655a4.382 4.382 0 0 0 4.394 0l14.864-8.655a4.545 4.545 0 0 0 2.198-3.803V55.538a4.27 4.27 0 0 0-.572-2.208 4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="a" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#9a03ff"/><stop offset="1" stop-color="#d90d1b" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="24" fill="#fff" filter="url(#b)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
-    string svgPartTwo = '</text></svg>';
+    string svgPartOne =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#a)" d="M0 0h270v270H0z"/><defs><filter id="b" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949a4.382 4.382 0 0 0-4.394 0l-10.081 6.032-6.85 3.934-10.081 6.032a4.382 4.382 0 0 1-4.394 0l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616 4.54 4.54 0 0 1-.608-2.187v-9.31a4.27 4.27 0 0 1 .572-2.208 4.25 4.25 0 0 1 1.625-1.595l7.884-4.59a4.382 4.382 0 0 1 4.394 0l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616 4.54 4.54 0 0 1 .608 2.187v6.032l6.85-4.065v-6.032a4.27 4.27 0 0 0-.572-2.208 4.25 4.25 0 0 0-1.625-1.595L41.456 24.59a4.382 4.382 0 0 0-4.394 0l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595 4.273 4.273 0 0 0-.572 2.208v17.441a4.27 4.27 0 0 0 .572 2.208 4.25 4.25 0 0 0 1.625 1.595l14.864 8.655a4.382 4.382 0 0 0 4.394 0l10.081-5.901 6.85-4.065 10.081-5.901a4.382 4.382 0 0 1 4.394 0l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616 4.54 4.54 0 0 1 .608 2.187v9.311a4.27 4.27 0 0 1-.572 2.208 4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721a4.382 4.382 0 0 1-4.394 0l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616 4.53 4.53 0 0 1-.608-2.187v-6.032l-6.85 4.065v6.032a4.27 4.27 0 0 0 .572 2.208 4.25 4.25 0 0 0 1.625 1.595l14.864 8.655a4.382 4.382 0 0 0 4.394 0l14.864-8.655a4.545 4.545 0 0 0 2.198-3.803V55.538a4.27 4.27 0 0 0-.572-2.208 4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="a" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#9a03ff"/><stop offset="1" stop-color="#d90d1b" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="24" fill="#fff" filter="url(#b)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
+    string svgPartTwo = "</text></svg>";
 
     // 'mapping' data type to store names
     mapping(string => address) public domains;
     // this mapping will store values
     mapping(string => string) public records;
+    mapping(uint256 => string) public names;
+
+    function getAllNames() public view returns (string[] memory) {
+        console.log("Getting all names from contract.");
+        string[] memory allNames = new string[](_tokenIds.current());
+        for (uint256 i = 0; i < _tokenIds.current(); i++) {
+            allNames[i] = names[i];
+            console.log("Name for token %d is %s", i, allNames[i]);
+        }
+
+        return allNames;
+    }
 
     // Gotta make the contract payable
-    constructor(string memory _tld) payable ERC721("GangGang Name Service", "GNS") {
+    constructor(string memory _tld)
+        payable
+        ERC721("GangGang Name Service", "GNS")
+    {
+        owner = payable(msg.sender);
         tld = _tld;
-        console.log(
-            "%s name service deployed", _tld
-        );
+        console.log("%s name service deployed", _tld);
     }
 
     // this will give us domain price based on length
-    function lengthPrice(string calldata name) public pure returns(uint) {
-        uint len = StringUtils.strlen(name);
+    function lengthPrice(string calldata name) public pure returns (uint256) {
+        uint256 len = StringUtils.strlen(name);
         require(len > 0);
         if (len == 3) {
             return 5 * 10**17; // 5 MATIC = 5 w/ 18 '0's. Going w/ 0.5 Matic b/c faucets
@@ -49,12 +71,18 @@ contract Domains is ERC721URIStorage{
         }
     }
 
+    function valid(string calldata name) public pure returns (bool) {
+        return StringUtils.strlen(name) >= 3 && StringUtils.strlen(name) <= 5;
+    }
+
     // register function that adds names to mapping
-    function register(string calldata name) public payable{
+    function register(string calldata name) public payable {
+        if (msg.sender != domains[name]) revert Unauthorized();
+        if (!valid(name)) revert InvalidName(name);
         // Check that the name is unregistered
         require(domains[name] == address(0));
 
-        uint _price = this.lengthPrice(name);
+        uint256 _price = this.lengthPrice(name);
 
         // Check if enough Matic was paid in txn
         require(msg.value >= _price, "Not enough MATIC!");
@@ -62,12 +90,19 @@ contract Domains is ERC721URIStorage{
         // Combine name with TLD
         string memory _name = string(abi.encodePacked(name, ".", tld));
         // Create SVG for NFT w/ name
-        string memory finalSvg = string(abi.encodePacked(svgPartOne, _name, svgPartTwo));
+        string memory finalSvg = string(
+            abi.encodePacked(svgPartOne, _name, svgPartTwo)
+        );
         uint256 newRecordId = _tokenIds.current();
         uint256 length = StringUtils.strlen(name);
-            string memory strLen = Strings.toString(length);
+        string memory strLen = Strings.toString(length);
 
-        console.log("Registering %s.%s on the contract with tokenID %d", name, tld, newRecordId);
+        console.log(
+            "Registering %s.%s on the contract with tokenID %d",
+            name,
+            tld,
+            newRecordId
+        );
 
         // Create JSON metadata for NFT: combine strings and -> base64
         string memory json = Base64.encode(
@@ -86,16 +121,23 @@ contract Domains is ERC721URIStorage{
             )
         );
 
-        string memory finalTokenUri = string( abi.encodePacked("data:application/json;base64,", json));
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
 
-            console.log("\n---------------------------------------------------------");
-            console.log("Final tokenURI", finalTokenUri);
-            console.log("---------------------------------------------------------\n");
+        console.log(
+            "\n---------------------------------------------------------"
+        );
+        console.log("Final tokenURI", finalTokenUri);
+        console.log(
+            "---------------------------------------------------------\n"
+        );
 
         _safeMint(msg.sender, newRecordId);
         _setTokenURI(newRecordId, finalTokenUri);
         domains[name] = msg.sender;
 
+        names[newRecordId] = name;
         _tokenIds.increment();
         console.log("%s has registered a domain!", msg.sender);
     }
@@ -106,12 +148,33 @@ contract Domains is ERC721URIStorage{
     }
 
     function setRecord(string calldata name, string calldata record) public {
+        if (msg.sender != domains[name]) revert Unauthorized();
         // Check that the owner is the txn sender
         require(domains[name] == msg.sender);
         records[name] = record;
     }
 
-    function getRecord(string calldata name) public view returns(string memory) {
+    function getRecord(string calldata name)
+        public
+        view
+        returns (string memory)
+    {
         return records[name];
+    }
+
+    modifier onlyOwner() {
+        require(isOwner());
+        _;
+    }
+
+    function isOwner() public view returns (bool) {
+        return msg.sender == owner;
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Failed to withdraw Matic");
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -3,22 +3,45 @@
 
 // Note: hre.ethers = Hardhat Runtime Environment
 const main = async () => {
+  const [owner, superCoder] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
   // Pass in our domain name, in this case: ganggang will init our tld
   const domainContract = await domainContractFactory.deploy("ganggang");
   await domainContract.deployed();
 
-  console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract owner:", owner.address);
 
-  // Passing two variables - domain name & value AKA $$m00lah$$
-  let txn = await domainContract.register("kitty", {value: hre.ethers.utils.parseEther('0.1')});
+  // BIG Payment! $
+  let txn = await domainContract.register("a16z", {value: hre.ethers.utils.parseEther('1234')});
   await txn.wait();
 
-  const addy = await domainContract.getAddress("kitty");
-  console.log("Owner of domain kitty:", addy);
-
+  // How much money is in here?
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
+  // Quick grab the funds!
+  try {
+    txn = await domainContract.connect(superCoder).withdraw();
+    await txn.wait();
+  }
+  catch(error){
+    console.log("Could not rob contract");
+  }
+
+  // Let's look in wallet to compare
+  let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+  console.log("Balance of owner before withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
+
+  // Oops, looks like the owner is saving their money!
+  txn = await domainContract.connect(owner).withdraw();
+  await txn.wait();
+
+  // Fetch balance of contract and owner
+  const contractBalance = await hre.ethers.provider.getBalance(domainContract.address);
+  ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+  console.log("Contract balance after withdrawal:", hre.ethers.utils.formatEther(contractBalance));
+  console.log("Balance of owner after withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
 };
 
 const runMain = async () => {
